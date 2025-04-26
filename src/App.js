ae8b6198f5d6e687ef9764c3874d0fea8a6c43c8
gopi <galigopigopi@gmail.com>
2025-04-26 20:45:33 +0530

    router changedd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ExerciseList from './components/ExerciseList';
 import EditExercise from './components/EditExercise';
